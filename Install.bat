624cc80c79ce9a89457536ff9abdfee3804e65e7
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2018-09-30 22:32:08 +0200

    Fix the install script

--- Install.bat
+++ Install.bat
@@ -1,11 +1,13 @@
 @ECHO OFF
 
+SET INCLUDE_PATH=%~dp0include\arduino-mock
+SET LIB_PATH=%~dp0build\dist\lib
 SET DESTINATION_PATH=C:\local\arduino-mock
 
 SET XCOPY_OPTIONS=/D /Y
 
-XCOPY "%~dp0build\include\arduino_mock\*.h" "%DESTINATION_PATH%\include" %XCOPY_OPTIONS%
-XCOPY "%~dp0build\dist\lib\Debug\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\debug" %XCOPY_OPTIONS%
-XCOPY "%~dp0build\dist\lib\Release\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\release" %XCOPY_OPTIONS%
-XCOPY "%~dp0build\dist\lib\MinSizeRel\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\release-min-size" %XCOPY_OPTIONS%
-XCOPY "%~dp0build\dist\lib\RelWithDebInfo\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\release-with-debug-info" %XCOPY_OPTIONS%
+XCOPY "%INCLUDE_PATH%\*.h" "%DESTINATION_PATH%\include" %XCOPY_OPTIONS%
+XCOPY "%LIB_PATH%\Debug\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\debug" %XCOPY_OPTIONS%
+XCOPY "%LIB_PATH%\Release\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\release" %XCOPY_OPTIONS%
+XCOPY "%LIB_PATH%\MinSizeRel\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\release-min-size" %XCOPY_OPTIONS%
+XCOPY "%LIB_PATH%\RelWithDebInfo\arduino_mock.lib" "%DESTINATION_PATH%\lib\vs12\release-with-debug-info" %XCOPY_OPTIONS%
