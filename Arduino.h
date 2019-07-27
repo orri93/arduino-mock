193e3d0e4d915c8ff8aa40f5f442bfbcc015191c
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2019-07-27 18:58:27 +0200

    Adding min and max and other macros from Arduino.h

--- Arduino.h
+++ Arduino.h
@@ -25,6 +25,29 @@ extern "C" {
 #define false 0x0
 #endif
 
+/*
+ * From Arduino.h starting
+ *
+ */
+// undefine stdlib's abs if encountered
+#ifdef abs
+#undef abs
+#endif
+
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define abs(x) ((x)>0?(x):-(x))
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+#define radians(deg) ((deg)*DEG_TO_RAD)
+#define degrees(rad) ((rad)*RAD_TO_DEG)
+#define sq(x) ((x)*(x))
+/*
+ * From Arduino.h end
+ *
+ */
+
+
 #define PI 3.1415926535897932384626433832795
 #define HALF_PI 1.5707963267948966192313216916398
 #define TWO_PI 6.283185307179586476925286766559
